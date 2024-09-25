3dbbbc954024b4d1151198734e185385a8c1d2ff
anthony <luxingchaode@gmail.com>
2024-09-25 21:11:22 +0800

    change to raw img

--- client/pages/demo.tsx
+++ client/pages/demo.tsx
@@ -3,7 +3,6 @@ import React from "react";
 
 import AppWrapper from "../components/AppWrapper";
 import { Col } from "../components/Layout";
-import Image from 'next/image'
 
 const { publicRuntimeConfig } = getConfig();
 
@@ -13,10 +12,8 @@ const TermsPage = () => (
     <Col width={600} maxWidth="97%" alignItems="flex-start">
       <p>模特春季服装,经典款</p>
 
-      <Image
+      <img
         src="/images/model-img.jpeg"
-        width={300}
-        height={500}
         alt="Picture of the author"
       />
     </Col>
