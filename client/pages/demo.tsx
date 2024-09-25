0b16f5fcfbb613290cd5e5797b4d3d8adb856916
anthony <luxingchaode@gmail.com>
2024-09-25 21:20:13 +0800

    remove header in demo page

--- client/pages/demo.tsx
+++ client/pages/demo.tsx
@@ -1,24 +1,20 @@
 import getConfig from "next/config";
 import React from "react";
 
-import AppWrapper from "../components/AppWrapper";
 import { Col } from "../components/Layout";
 
 const { publicRuntimeConfig } = getConfig();
 
 const TermsPage = () => (
-  <AppWrapper>
-    {/* TODO: better container */}
-    <Col width={600} maxWidth="97%" alignItems="flex-start">
-      <p>模特春季服装,经典款</p>
+  <Col width={600} maxWidth="97%" alignItems="flex-start">
+    <p>模特春季服装,经典款</p>
 
-      <img
-        src="/images/model-img.jpeg"
-        alt="Picture of the author"
-        style={{maxWidth: "100%"}}
-      />
-    </Col>
-  </AppWrapper>
+    <img
+      src="/images/model-img.jpeg"
+      alt="Picture of the author"
+      style={{maxWidth: "100%"}}
+    />
+  </Col>
 );
 
 export default TermsPage;
